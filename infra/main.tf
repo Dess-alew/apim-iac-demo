773f2870e59e37cccc3816c88701037147617f92
Desalew <dessalew186@gmail.com>
2026-05-16 08:09:31 -0500

    fix subid

--- infra/main.tf
+++ infra/main.tf
@@ -1,10 +1,6 @@
 provider "azurerm" {
   features {}
-  subscription_id = var.subscription_id
-}
-
-variable "subscription_id" {
-  default = ""
+  subscription_id = "${{ secrets.AZURE_SUBSCRIPTION_ID }}"
 }
 
 resource "azurerm_resource_group" "rg" {
@@ -13,10 +9,16 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_api_management" "apim" {
-  name                = "my-apim-demo"
+  name                = "apim-dess-${random_string.suffix.result}"
   location            = "East US"
   resource_group_name = azurerm_resource_group.rg.name
   publisher_name      = "My Org"
   publisher_email     = "admin@example.com"
   sku_name            = "Developer_1"
+}
+
+resource "random_string" "suffix" {
+  length  = 6
+  special = false
+  upper   = false
 }
